8b21bbe356f408a571264d9fa35eef0f786fa41e
2DavidGarcia4 <gacub20ru@gmail.com>
2022-11-12 14:18:39 -0600

    add console log

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 const config_1 = require("./config");
+console.log(config_1.envConfig);
 app_1.app.listen(config_1.envConfig.port, () => {
     console.log('Server is runing in port:', config_1.envConfig.port);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { app } from "./app";
 import { envConfig } from "./config"
+console.log(envConfig)
 
 app.listen(envConfig.port, () => {
   console.log('Server is runing in port:', envConfig.port)
